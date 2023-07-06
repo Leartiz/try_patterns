8807d8eb2b1b2b927d1bbbb4d12832ea671b8cae
Артемьев Леонид Сергеевич <leonidart14@gmail.com>
2023-07-06 09:35:05 +0700

    upd example csh from book

--- dps_csh/exps/n1/main.cs
+++ dps_csh/exps/n1/main.cs
@@ -1,4 +1,4 @@
-﻿namespace Delegate
+﻿namespace N1
 {
     interface ILogSaver
     {
@@ -6,28 +6,73 @@
         void UploadExceptions(IEnumerable<string> exceptions);
     }
 
-    class Channel : ILogSaver
+    // *** конкретная реализация интерфейса
+
+    public class Channel : ILogSaver
     {
+        private Random rnd = new Random();
+        private bool hasError()
+        {
+            return rnd.Next(0, 2) == 1;
+        }
+
+        // ***
+
         public void UploadLogEntries(IEnumerable<string> logEntries)
         {
-            Console.WriteLine("Channel, UploadLogEntries.");
+            if (hasError())
+            {
+                throw new CommunicationException(new SystemException());
+            }
+
+            Console.WriteLine("Channel, UploadLogEntries:");
+            Console.WriteLine("{ ");
+            foreach (var one in logEntries)
+            {
+                Console.WriteLine(one);
+            }
+            Console.WriteLine("}");
         }
         public void UploadExceptions(IEnumerable<string> exceptions)
         {
-            Console.WriteLine("Channel, UploadExceptions.");
+            if (hasError())
+            {
+                throw new CommunicationException(new SystemException());
+            }
+
+            Console.WriteLine("Channel, UploadExceptions:");
+            Console.WriteLine("{ ");
+            foreach (var one in exceptions)
+            {
+                Console.WriteLine(one);
+            }
+            Console.WriteLine("}");
         }
     }
 
-    class CommunicationException : SystemException { }
-    class OperationFailedException : SystemException
+    // *** исключения
+
+    public class CommunicationException : SystemException 
+    {
+        public CommunicationException(SystemException se)
+        {
+            Console.WriteLine("CommunicationException, ctor, msg: " + se.Message);
+        }
+    }
+    public class OperationFailedException : SystemException
     {
         public OperationFailedException(SystemException se) 
-        { 
-
+        {
+            Console.WriteLine("OperationFailedException, ctor, msg: " + se.Message);
         }
     }
 
-    class ClientBase<T>
+    // ***
+    /* заметки:
+       - возможно не хватает виртуального метода (реализация ILogSaver без внедрения)
+    */
+
+    public class ClientBase<T>
     {
         protected Channel Channel = new Channel();
 
@@ -41,16 +86,24 @@
         }
     }
 
+    // ***
+    /* заметки:
+       - не вижу в коде внедрения зависимостей (конкретно делегата/функции)
+    */
 
-    class LogSaverProxy : ILogSaver
+    public class LogSaverProxy : ILogSaver
     {
         class LogSaverClient : ClientBase<ILogSaver>
         {
+            // должен быть виртуальным?
             public ILogSaver LogSaver
             {
                 get { return Channel; }
             }
         }
+
+        // ***
+
         public void UploadLogEntries(IEnumerable<string> logEntries)
         {
             UseProxyClient((ILogSaver c) => c.UploadLogEntries(logEntries));
@@ -65,7 +118,7 @@
 
         private void UseProxyClient(Action<ILogSaver> accessor)
         {
-            var client = new LogSaverClient();
+            var client = new LogSaverClient(); // DI?
             try
             {
                 accessor(client.LogSaver);
@@ -79,13 +132,46 @@
         }
     }
 
-    public class Exp
+    public class Program
     {
         static int Main(string[] args)
         {
             {
                 var foo = (int x) => Console.WriteLine(x);
                 foo(100);
+            }
+            // ***
+            {
+                try
+                {
+                    LogSaverProxy logSaverProxy = new LogSaverProxy();
+                    string[] days = {
+                        "Monday", "Tuesday", "Wednesday",
+                        "Thursday", "Friday", "Saturday",
+                        "Sunday"
+                    };
+                    logSaverProxy.UploadLogEntries(days);
+                }
+                catch(SystemException se)
+                {
+                    Console.WriteLine("Program, Main, msg: " + se.Message);
+                }                
+            }
+            {
+                try
+                {
+                    LogSaverProxy logSaverProxy = new LogSaverProxy();
+                    string[] days = {
+                        "Monday", "Tuesday", "Wednesday",
+                        "Thursday", "Friday", "Saturday",
+                        "Sunday"
+                    };
+                    logSaverProxy.UploadExceptions(days);
+                }
+                catch (SystemException se)
+                {
+                    Console.WriteLine("Program, Main, msg: " + se.Message);
+                }
             }
 
             return 0;
