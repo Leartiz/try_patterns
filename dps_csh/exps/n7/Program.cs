6f75545b54c1f289170306da6d9c34d28d0abce9
Артемьев Леонид Сергеевич <leonidart14@gmail.com>
2023-07-10 16:57:24 +0700

    add exmpl csh

---
+++ dps_csh/exps/n7/Program.cs
@@ -0,0 +1,23 @@
+﻿Test();
+GC.Collect();   // очистка памяти под объект tom
+Console.Read(); // ставим задержку
+
+Console.WriteLine(GC.GetTotalMemory(false));
+
+void Test()
+{
+    Person tom = new Person("Tom");
+
+    Console.WriteLine("gen: " + GC.GetGeneration(tom));
+}
+
+public class Person
+{
+    public string Name { get; }
+    public Person(string name) => Name = name;
+
+    ~Person()
+    {
+        Console.WriteLine($"{Name} has been deleted");
+    }
+}
