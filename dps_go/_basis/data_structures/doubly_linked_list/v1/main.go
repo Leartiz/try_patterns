4701d8efb08b8a98d31e6ad8f1de8595e5b7c2e0
Артемьев Леонид Сергеевич <l.artemyev@zota.ru>
2024-07-30 16:59:20 +0700

    some

--- dps_go/_basis/data_structures/doubly_linked_list/v1/main.go
+++ dps_go/_basis/data_structures/doubly_linked_list/v1/main.go
@@ -1,1 +1,20 @@
 package main
+
+type Node[T any] struct {
+	value      T
+	prev, next *Node[T]
+}
+
+func NewNode[T any](value T) *Node[T] {
+	return &Node[T]{
+		value: value,
+		prev:  nil,
+		next:  nil,
+	}
+}
+
+type List[T any] struct {
+	beg, end *Node[T]
+}
+
+// -----------------------------------------------------------------------
